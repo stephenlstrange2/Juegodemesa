b718d137b56f5566bc5a614e4394d87483cc9bb7
Stephen Lincoln Strange <stephenlstrange2@gmail.com>
2022-03-20 15:09:43 -0600

    Create str.h

---
+++ src/str.h
@@ -0,0 +1,58 @@
+#include <iostream>
+
+
+class specsJugadores{
+    private:
+        string nombre;
+        int aFvelocidad;
+        int aFpoder;
+        int aMcordura;
+        int aMconocimiento;
+        int turno;
+        int danoRecibido;
+        int salud;
+        int dados;
+    
+    public:
+//         void nuevoJugador(){
+//             nombre = strNombre;
+//             direccion = strDireccion;
+//             placa = strPlaca;
+//             fecha = strFecha;
+//         }
+
+//     // Setter
+//         void setNombre(string strNombre){
+//             nombre = strNombre;
+//         }        
+//         void setDireccion(string strDireccion){
+//             direccion = strDireccion;
+//         }
+//         void setPlaca(string strPlaca){
+//             placa = strPlaca;
+//         }
+//         void setFecha(string strFecha){
+//             fecha = strFecha;
+//         }
+
+    // Getter
+//         string getNombre(){
+//             return nombre;
+//         }        
+//         string getDireccion(){
+//             return direccion;
+//         }
+//         string getPlaca(){
+//             return placa;
+//         }
+//         string getFecha(){
+//             return fecha;
+//         }
+//         string getAll(){
+//             string buffer = "";
+//             buffer+= "Nombre: "+nombre+" Direccion: "+direccion;
+//             buffer+= " Placa: "+placa+" Fecha de inicio de residencia: "+fecha+"\n"; 
+//             return buffer;
+//         }
+};
+
