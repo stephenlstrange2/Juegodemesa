f7265170f37dd311850dea64fc85d902de68beb0
stephenlstrange2 <stephenlstrange2@gmail.com>
2022-03-21 18:43:18 -0600

    Agregar dados

--- src/main.cpp
+++ src/main.cpp
@@ -11,11 +11,14 @@ Primero llamamos a las librerias y headers de cajon
 #include <ctime>
 #include <random>
 using namespace std;
-
+string eformat = "                                                             ";
 #include "str.h"
 
 #include <stdio.h>
 
+
+specsJugadores jugador;
+int buffer;
 void inicializar(){
 
     std::random_device dev;
@@ -25,7 +28,46 @@ void inicializar(){
     srand(semilla);
 }
 
-void main(){
+void pintarMenu(){
+    int flag = 1;
+    do
+    {
+        system("clear");
+        std::cout << eformat << "MENU\n\n"<< eformat <<"1.Iniciar Juego\n"<<eformat<<"2.Salir\n";
+        std::cin >> buffer;
+        if(buffer==1){
+            system("clear");
+            std::cout << eformat << "Elige a tu jugador\n\n"<< eformat <<"1.Brandon Jaspers\n"<<eformat<<"2.Zoe Ingstrom\n"<< eformat <<"3.Father Rhinohardt\n"<<eformat<<"4.Salir al menu anterior\n";
+            std::cin >> buffer;
+            switch (buffer)
+            {
+            case 1:
+                std::cout << "Brandon Jaspers\n";
+                jugador.tirarDados(6);
+                flag = 0;
+                break;
+            
+            case 2:
+                std::cout << "Zoe Ingstrom";
+                flag = 0;
+                /* code */
+                break;
+            case 3:
+                std::cout << "Father Rhinohardt";
+                flag = 0;
+                /* code */
+                break;
+            default:
+            std::cout << "Respuesta no valida.\n";
+                break;
+            }
+        }
+    } while (flag);
+}
 
+int main(){
+    inicializar();
+    pintarMenu();
+    return 0;
 
 }
--- src/str.h
+++ src/str.h
@@ -1,5 +1,6 @@
 #include <iostream>
-
+#include <chrono>
+#include <thread>
 
 class specsJugadores{
     private:
@@ -12,11 +13,63 @@ class specsJugadores{
         int danoRecibido;
         int salud;
         int dados;
-    
+        void animacionDados(){
+            for (int i = 0; i < rand()%40; i++)
+            {
+            std::cout << "    _______\n  /\\       \\\n /  \\       \\\n/    \\_______\\\n\\    /       /\n \\  /       /\n  \\/_____  /\n";
+            std::this_thread::sleep_for(std::chrono::milliseconds(i*30));
+            system("clear");
+            std::cout << "    _______\n  /\\       \\\n /  \\   ()  \\\n/ () \\_______\\\n\\    /       /\n \\  /   ()  /\n  \\/_______/\n";
+            std::this_thread::sleep_for(std::chrono::milliseconds(i*30));
+            system("clear");
+            std::cout << "    _______\n  /\\ ()    \\\n /()\\       \\\n/    \\_____()\\\n\\    /()     /\n \\()/       /\n  \\/_____()/\n";      
+            std::this_thread::sleep_for(std::chrono::milliseconds(i*30));
+            system("clear");
+            }
+        }
     public:
-        void tirarDados(int canttiros){
-
+        int tirarDados(int canttiros){
+            if (canttiros <=  0 || canttiros > 6) return -1;
+            dados = 0;
+            int estado[] = {0,1,2};
+            for (int i = 0; i < canttiros; i++)
+            {
+                int _estado = estado[rand()%3];
+                std::cout<< eformat <<"TIRANDO DADOS";
+                sleep(3);
+                animacionDados();
+                std::cout << "Tu dado numero "<< i+1 <<" fue un \n";
+                switch (_estado)
+                {
+                case 0:
+                    std::cout << "    _______\n  /\\       \\\n /  \\       \\\n/    \\_______\\\n\\    /       /\n \\  /       /\n  \\/_____  /\n";
+                    sleep(3);
+                    break;
+                
+                case 1:
+                    std::cout << "    _______\n  /\\       \\\n /  \\   ()  \\\n/ () \\_______\\\n\\    /       /\n \\  /   ()  /\n  \\/_______/\n";
+                    dados++;
+                    sleep(3);
+                    break;
+                
+                case 2:
+                    std::cout << "    _______\n  /\\ ()    \\\n /()\\       \\\n/    \\_____()\\\n\\    /()     /\n \\()/       /\n  \\/_____()/\n";
+                    dados+=2;
+                    sleep(3);
+                    break;
+                default:
+                    std::cout<<"Error fatal, STD RAND NOT WORKING";
+                    break;
+                }
+            }
+            std::cout << "\nDADOS TIRADOS: "<< canttiros+1 <<"\nPuntos Acumulados: "<<dados << "\n"; 
             
+        }
+        int tomarCartaMapa(){
+            return 1;
+        } 
+        bool maldicion(){
+            return false;
         }
 //         void nuevoJugador(){
 //             nombre = strNombre;
